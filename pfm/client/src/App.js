95ba18a0e51ed04bad3e035106663b1eb16deff2
Michelle <michelle.douma@gmail.com>
2022-10-18 17:17:33 -0400

    updating file format

---
+++ pfm/client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./components/Nav/nav";
+import Contact from "./components/Contact/contact";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Nav />
+			<Contact />
+		</>
+	);
+}
+
+export default App;
